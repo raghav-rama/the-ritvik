4f7999e0e1dde3a11797be1ae97acbb69ed7ec00
raghav-rama <hackerer528@gmail.com>
2024-11-09 00:01:52 +0530

    fix: styling for mobile device

--- app/src/components/SanityImage.svelte
+++ app/src/components/SanityImage.svelte
@@ -16,19 +16,30 @@
 </script>
 
 <figure>
-	<img src={urlFor(value.asset).url()} alt={value.alt} width={width * 0.1} height={height * 0.1} />
+	<img src={urlFor(value.asset).width(width).height(height).url()} alt={value.alt} />
 	<figcaption>{value.caption}</figcaption>
 </figure>
 
 <style>
+	figure {
+		margin: var(--space-1) 0;
+		width: 100%;
+	}
+
 	img {
 		display: block;
-		margin: var(--space-1) auto;
+		max-width: 100%;
+		height: auto;
+		margin: 0 auto;
+		box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
 	}
+
 	figcaption {
 		text-align: center;
 		font-size: var(--font-size-1);
 		line-height: var(--line-height-0);
 		opacity: 0.7;
+		padding: 0 1rem;
+		margin-top: 5px;
 	}
 </style>
